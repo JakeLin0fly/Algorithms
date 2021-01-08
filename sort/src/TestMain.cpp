e00a66c36daf07cc622c6e21158c8d7ce90d51ee
JakeLin <jakelin0fly@gmail.com>
2021-01-08 22:34:55 +0800

    归并排序\快速排序

---
+++ sort/src/TestMain.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <vector>
+#include "algo_sort.h"
+
+using namespace std;
+
+template<class Type>
+void show(vector<Type>& nums) {
+    for (auto& val : nums) {
+        std::cout << val << " ";
+    }
+    cout << endl;
+}
+
+void pleaseInput() {
+    cout << "1. 冒泡排序\n"
+        << "2. 选择排序\n"
+        << "3. 插入排序\n"
+        << "4. 希尔排序\n"
+        << "5. 归并排序\n"
+        << "6. 快速排序\n"
+        << "7. 堆排序\n"
+        << "8. 计数排序\n"
+        << "9. 桶排序\n"
+        << "10. 基数排序\n"
+        << "0. 退出\n";
+}
+
+int main() {
+    while (true) {
+        vector<int> nums = { 2, 53, 32, 32, 3, 2, 435, 6, 9, 45, 44, 7, 13, 39, 66 };
+        int func;
+        pleaseInput();
+        cin >> func;
+        cout << "排序前："; show(nums);
+        switch (func) {
+        case 1:
+            cout << "冒泡排序: ";
+            bubble_sort(nums);
+            break;
+        case 2:
+            cout << "选择排序: ";
+            select_sort(nums);
+            break;
+        case 3:
+            cout << "插入排序: ";
+            insert_sort(nums);
+            break;
+        case 4:
+            cout << "希尔排序: ";
+            shell_sort(nums);
+            break;
+        case 5:
+            cout << "归并排序: ";
+            merge_sort(nums);
+            break;
+        case 6:
+            cout << "快速排序: ";
+            quick_sort(nums);
+            break;
+        case 7:
+            cout << "堆排序: ";
+            heap_sort(nums);
+            break;
+        case 8:
+            cout << "计数排序: ";
+            counting_sort(nums);
+            break;
+        case 9:
+            cout << "桶排序: ";
+            bucket_sort(nums);
+            break;
+        case 10:
+            cout << "基数排序: ";
+            radix_sort(nums);
+            break;
+        case 0:
+            return 0;
+        default:;
+        }
+        show(nums);
+    }
+
+    return 0;
+}
--- sort/src/algo_sort.h
+++ sort/src/algo_sort.h
@@ -1,59 +1,37 @@
-### 排序算法
+//
+// Created by JakeLin on 2021/1/8.
+//
 
-排序算法可以分为内部排序和外部排序，**内部排序**是数据记录在内存中进行排序，而**外部排序**是因排序的数据很大，一次不能容纳全部的排序记录，在排序过程中需要访问外存。【*图片来自网络*】
+#ifndef SORT_ALGO_SORT_H
+#define SORT_ALGO_SORT_H
+#include <iostream>
+#include <vector>
+using namespace std;
 
-![sort](./img/sort.png)
-
-![sort2](./img/sort2.png)
-
-#### 冒泡排序
-
-> 顾名思义，就像一堆气泡，大的气泡往上冒，一次遍历完的结果是待排序的气泡中最大的气泡冒出到有序序列。
-
-【步骤】
-
-1. 从第一个元素开始比较相邻元素，若前者更大，则交换这两个元素；
-2. 一次遍历完，一个元素完成有序；
-3. 重复第一步，直到所有元素有序。
-
-【**优化**】**当一次遍历结束，没有发生交换，整个序列便已完成排序。**
-
-```cpp
 /**
  * 冒泡排序
  * 稳定性：稳定
  * 时间复杂度：O(n*n)
  * 空间复杂度：O(1)
 **/
-template<class T>
-void bubble_sort(vector<T>& nums){
+template<class Type>
+void bubble_sort(vector<Type>& nums) {
     int n = nums.size();
     bool flag = false; // 优化 一次遍历是否发生交换
-    while(n > 1){
-        for(int i = 0; i+1 < n; ++i){
-            if(nums[i] > nums[i+1]){
-                swap(nums[i], nums[i+1]);
+    while (n > 1) {
+        for (int i = 0; i + 1 < n; ++i) {
+            if (nums[i] > nums[i + 1]) {
+                swap(nums[i], nums[i + 1]);
                 flag = true;
             }
         }
-        if(!flag)	break;
+        if (!flag)	break;
         flag = false;
         --n;
     }
 }
-```
 
-#### 选择排序
 
-> 每次遍历，选择待排序元素中最大元素，放置到待排序子数组最后位置。一次遍历至多只进行交换一次。
-
-【步骤】
-
-1. 遍历待排序子数组，选择最大待排序元素；
-2. 将待排序最大元素放置在待排序子数组最后位置；
-3. 重复第一步，直到所有元素有序。
-
-```cpp
 /**
  * 选择排序
  * 稳定性：不稳定（可能打破原来元素的相对位置）
@@ -61,30 +39,19 @@ void bubble_sort(vector<T>& nums){
  * 空间复杂度：O(1)
 **/
 template<class Type>
-void select_sort(vector<Type>& nums){
+void select_sort(vector<Type>& nums) {
     int n = nums.size();
-    while(n > 1){
+    while (n > 1) {
         int max_index = 0;
-        for(int i = 1; i < n; ++i){
-            if(nums[max_index] < nums[i])
+        for (int i = 1; i < n; ++i) {
+            if (nums[max_index] < nums[i])
                 max_index = i;
         }
-        swap(nums[max_index], nums[n-1]);
+        swap(nums[max_index], nums[n - 1]);
         --n;
     }
 }
-```
 
-#### 插入排序
-
-> 将待排序元素依次插入有序序列
-
-【步骤】
-
-1. 将第一个元素视为有序序列，往后为待排序序列；
-2. 依次向有序序列插入元素到合适的位置。
-
-```cpp
 /**
  * 插入排序
  * 稳定性：稳定
@@ -92,36 +59,23 @@ void select_sort(vector<Type>& nums){
  * 空间复杂度：O(1)
 **/
 template<class Type>
-void insert_sort(vector<Type>& nums){
+void insert_sort(vector<Type>& nums) {
     int n = nums.size();
-    for(int ordered = 1; ordered < n; ++ordered){
+    for (int ordered = 1; ordered < n; ++ordered) {
         Type value = nums[ordered];
         int i = ordered - 1;
-        while(i >= 0 && nums[i] > value){
-            nums[i+1] = nums[i];
+        while (i >= 0 && nums[i] > value) {
+            nums[i + 1] = nums[i];
             --i;
         }
-        nums[i+1] = value;
+        nums[i + 1] = value;
     }
 }
-```
 
-#### 希尔排序
-
-> 也称递减增量排序算法，希尔排序是基于插入排序的以下两点性质而提出改进方法的：
->
-> - 插入排序在对几乎已经排好序的数据操作时，效率高，即可以达到线性排序的效率；
-> - 但插入排序一般来说是低效的，因为插入排序每次只能将数据移动一位；
-
-【图解】
-
-参考：https://www.cnblogs.com/chengxiao/p/6104371.html
-
-```cpp
 /**
  * 希尔排序
  * 稳定性：不稳定（可能打破原来元素的相对位置）
- * 时间复杂度：O(nlogn)
+ * 时间复杂度：O(n*logn)
  * 空间复杂度：O(1)
 **/
 template<class Type>
@@ -140,17 +94,7 @@ void shell_sort(vector<Type>& nums) {
         }
     }
 }
-```
 
-#### 归并排序
-
-> 归并排序采用分而治之的思想。将原序列均分为两个子序列，选排序子序列，再合并有序子序列。
-
-【图解】
-
-参考：https://www.cnblogs.com/chengxiao/p/6194356.html
-
-```cpp
 /**
  * 归并排序
  * 稳定性：稳定
@@ -189,17 +133,9 @@ template<class Type>
 void merge_sort(vector<Type>& nums){
     _sort(nums, 0, nums.size() - 1);
 }
-```
 
-#### 快速排序
 
-【步骤】
 
-1. 待排序列中取一个“基准”
-2. 所有比“基准”小的元素放”基准“前面，所有比“基准”大的元素放“基准”后面。
-3. 递归处理基准两侧的子序列。
-
-```cpp
 /**
  * 快速排序
  * 稳定性：不稳定（可能打破原来元素的相对位置）
@@ -231,18 +167,51 @@ template<class Type>
 void quick_sort(vector<Type>& nums) {
     _quick_sort(nums, 0, nums.size() - 1);
 }
-```
 
 
+/**
+ * 堆排序
+ * 稳定性：不稳定（可能打破原来元素的相对位置）
+ * 时间复杂度：O(n*logn)
+ * 空间复杂度：O(1)
+**/
+template<class Type>
+void heap_sort(vector<Type>& nums) {
+    int n = nums.size();
+}
 
-#### 堆排序
+/**
+ * 计数排序
+ * 稳定性：稳定
+ * 时间复杂度：O(n+m)
+ * 空间复杂度：O(m)
+**/
+template<class Type>
+void counting_sort(vector<Type>& nums) {
+    int n = nums.size();
+}
 
-#### 计数排序
+/**
+ * 桶排序
+ * 稳定性：稳定
+ * 时间复杂度：O(n+m)
+ * 空间复杂度：O(n+m)
+**/
+template<class Type>
+void bucket_sort(vector<Type>& nums) {
+    int n = nums.size();
+}
 
-#### 桶排序
+/**
+ * 基数排序
+ * 稳定性：稳定
+ * 时间复杂度：O(n*m)
+ * 空间复杂度：O(n+m)
+**/
+template<class Type>
+void radix_sort(vector<Type>& nums) {
+    int n = nums.size();
+}
 
-#### 基数排序
 
-
-
-#### 
+#endif //SORT_ALGO_SORT_H
